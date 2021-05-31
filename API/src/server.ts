8ee5d0411efa40f8cae89e7f51fc480d7df39671
Hiury Lima <limahiury6@gmail.com>
2021-05-31 02:44:38 -0300

    Desenvolvimento inicial
    Criação do projeto com Node.js, após criação das primeiras rotas GET, POST e também foi feito o teste das mesmas utilizando o Insominia.

---
+++ API/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+// http://localhost:3333/users
+app.get("/", (request, response) => {
+    return response.json({message: "Hello World - NLW04"})
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "Os dados foram salvos com sucesso!"})
+});
+
+app.listen(3333, () => console.log('Server is running!'));
